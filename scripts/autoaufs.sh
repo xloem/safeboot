f8fff7fec7ed1609851e46b32bd660f7a29972e9
Karl Semich <0xloem@gmail.com>
2015-01-03 15:07:44 -0500

    overlay folders from both partitions

--- scripts/autoaufs.sh
+++ scripts/autoaufs.sh
@@ -2,31 +2,44 @@
 
 # manages an increasing list of overlays using aufs
 
+# usage: $0 mountpath basedir overlaysdir [overlaysdir ...]
+
 # TODO: the top overlay should be on a different partition from the bottom overlays (move them over)
 # the partition containing the bottom overlays should be remounted read-only prior to mount
 
+DESTINATION="$1"
+shift
 BASE="$1"
-OVERLAYSPATH="$2"
-DESTINATION="$3"
+shift
 
-mkdir -p "$OVERLAYSPATH" "$DESTINATION"
+#BASE="$1"
+#OVERLAYSPATH="$2"
+#DESTINATION="$3"
+
+mkdir -p "$DESTINATION"
 
 BRSTR="$BASE=rr"
 
-cd "$OVERLAYSPATH"
-if [ -d "0" ]; then
+for OVERLAYSPATH in "$@"; do
+	mkdir -p "$OVERLAYSPATH" || continue
+
+	cd "$OVERLAYSPATH"
 	for overlay in *; do
-		BRSTR="$overlay=rr:$BRSTR"
+		if [ "$overlay" = '*' ]; then
+			overlay=0
+		else
+			BRSTR="$overlay=rr:$BRSTR"
+		fi
 	done
-	# remove any prefixed zeros
-	overlay=$(echo $overlay | sed 's/^0*//')
-	# add 1
-	overlay=$((overlay+1))
-	# prefix 0s
-	overlay=$(printf %0.6d $overlay)
-else
-	overlay=0
-fi
+done
+
+# remove any prefixed zeros
+overlay=$(echo $overlay | sed 's/^0*//')
+# add 1
+overlay=$((overlay+1))
+# prefix 0s
+overlay=$(printf %0.6d $overlay)
+
 mkdir -p "$overlay"
 
 mount -t aufs -o "br:$overlay:$BRSTR" "$OVERLAYSPATH" "$DESTINATION"
